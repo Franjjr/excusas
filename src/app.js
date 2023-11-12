20c70e5b44c1ad2482425d167e41d1bee23c23ad
Fran Julian <138630369+Franjjr@users.noreply.github.com>
2023-11-12 16:43:01 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,15 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+let quien =['Mi perro ','Mi abuela ','Una tortuga ','Un pajaro '];
+let accion = ['se comio ','perdio ','rompio ', 'regalo'];
+let que = ['mi trabajo ','las llaves de casa ','las llaves del coche ','el gps '];
+let donde = ['antes de salir de casa', 'en el parque','justo cuando venia aqui','durante el almuerzo'];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+let numarray0 = Math.floor(Math.random() * ( 3 - 0) + 0);
+let numarray1 = Math.floor(Math.random() * ( 3 - 0) + 0);
+let numarray2 = Math.floor(Math.random() * ( 3 - 0) + 0);
+let numarray3 = Math.floor(Math.random() * ( 3 - 0) + 0);
+let excusa = quien[numarray0] + accion[numarray1] + que[numarray2] + donde[numarray3];
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+
+function frase(){
+    document.getElementById("excuse").innerHTML = excusa;
+}
