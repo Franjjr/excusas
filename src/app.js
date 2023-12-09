47997627bf6dd5c68027e892febc786ee598b337
Franjjr <franjjr@gmail.com>
2023-12-09 01:09:17 +0100

    Feat: Se agrego el boton y ahora se cambio la funcion frase para que ahora cre excusas sin fin

--- src/app.js
+++ src/app.js
@@ -3,13 +3,12 @@ let accion = ['se comio ','perdio ','rompio ', 'regalo'];
 let que = ['mi trabajo ','las llaves de casa ','las llaves del coche ','el gps '];
 let donde = ['antes de salir de casa', 'en el parque','justo cuando venia aqui','durante el almuerzo'];
 
-let numarray0 = Math.floor(Math.random() * ( 3 - 0) + 0);
-let numarray1 = Math.floor(Math.random() * ( 3 - 0) + 0);
-let numarray2 = Math.floor(Math.random() * ( 3 - 0) + 0);
-let numarray3 = Math.floor(Math.random() * ( 3 - 0) + 0);
-let excusa = quien[numarray0] + accion[numarray1] + que[numarray2] + donde[numarray3];
-
-
 function frase(){
+    let numarray0 = Math.floor(Math.random() * ( 3 - 0) + 0);
+    let numarray1 = Math.floor(Math.random() * ( 3 - 0) + 0);   
+    let numarray2 = Math.floor(Math.random() * ( 3 - 0) + 0);
+    let numarray3 = Math.floor(Math.random() * ( 3 - 0) + 0);
+    let excusa = quien[numarray0] + accion[numarray1] + que[numarray2] + donde[numarray3];
+
     document.getElementById("excuse").innerHTML = excusa;
 }
